cb86a2114c4be5cc9e32b9f29f5a7df2fdce8348
mizturgan <izturganovmahmut4@gmail.com>
2025-11-06 20:32:26 +0500

    s

--- internal/web/static/js/filter.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('apply-filter').addEventListener('click', () => {
-    const checked = Array.from(document.querySelectorAll('input[name="category"]:checked'))
-        .map(cb => cb.value);
-    
-    // Имитируем результат фильтрации
-    document.getElementById('filter-results').innerHTML =
-        `<p>Выбранные категории: ${checked.join(', ')}</p>`;
-});
